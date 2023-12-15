b622a865b2e75767c8062dc7b6c31e8dbdb02437
gbox3d <gbox3d@gmail.com>
2023-12-15 13:55:24 +0900

    .....

--- pytorch/cifar_10_transform.py
+++ pytorch/cifar_10_transform.py
@@ -4,25 +4,28 @@ import torch
 import torchvision.transforms as T
 
 from torchvision.datasets.cifar import CIFAR10
+from torchvision.transforms import RandomHorizontalFlip, RandomCrop, Normalize
 #%%
-_transform = T.Compose([
-    # T.ToPILImage(),
+_transforms = T.Compose([
+    T.ToPILImage(),
     T.RandomCrop((32,32), padding=4),
     T.RandomHorizontalFlip(p=0.5),
-    T.ToTensor()
+    T.ToTensor(),
+    Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.247, 0.243, 0.261)),
+   T.ToPILImage()
 ])
 
-cmp_training_data = CIFAR10(root="./datasets", train=True, download=True, transform=_transform)
-cmp_test_data = CIFAR10(root="./datasets", train=False, download=True, transform=_transform)
-#%% 변형본 출력 
-print(cmp_training_data[0][0].shape)
+cmp_training_data = CIFAR10(root="./datasets", train=True, download=True, transform=_transforms)
+cmp_test_data = CIFAR10(root="./datasets", train=False, download=True, transform=_transforms)
+
+#%%
 for i in range(10):
-    plt.subplot(2, 5, i+1)
-    # plt.imshow(cmp_training_data[i].permute(1, 2, 0))
-    plt.imshow(cmp_training_data[i][0].permute(1, 2, 0))
-    # print(cmp_training_data[i][1])
-    
+   plt.subplot(2, 5, i+1)
+   plt.imshow(_transforms(cmp_training_data.data[i]))
 plt.show()
+
+#%% 변형본 출력 
+print(cmp_training_data.data[0].shape)
 
 # %% 원본 출력
 for i in range(10):
@@ -30,3 +33,5 @@ for i in range(10):
     plt.imshow(cmp_training_data.data[i])
 plt.show()
 
+
+# %%
